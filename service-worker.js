bf2a40aab874365b3a3724002ad9333dbc9328a8
4000D <dkdkajej@gmail.com>
2020-04-06 16:53:33 +0900

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Onther-Tech/mton-migrator/precache-manifest.4d8a062850c6e67158dde7664ebebb9f.js"
+  "/mton-migrator/precache-manifest.6dd399f66bf3c87c85692dc6e4efba7f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Onther-Tech/mton-migrator/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mton-migrator/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
